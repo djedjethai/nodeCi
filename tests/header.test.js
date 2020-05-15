b9dbf76247071c31505786d62a2e2b31145a93ff
djedjethai <djedje.thai.ok@gmail.com>
2020-05-15 15:22:53 +0700

    first test

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,5 +1,19 @@
+const puppeteer = require('puppeteer');
+
 test('adds 2 numbers', () => {
     const sum = 1 + 2;
     
     expect(sum).toEqual(3); 
+});
+
+test('We can lunch a browser', async () => {
+    jest.setTimeout(300000);
+    const browser = await puppeteer.launch({
+        headless: false
+    });
+    const page = await browser.newPage();
+    await page.goto('localhost:3000');
+
+    const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+    expect(text).toEqual('Blogster');
 })
