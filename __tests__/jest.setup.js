d4196a1d9f141147d0551627c4d836614cf13582
djedjethai <djedje.thai.ok@gmail.com>
2020-07-25 16:19:05 +0700

    testq1

--- __tests__/jest.setup.js
+++ __tests__/jest.setup.js
@@ -3,7 +3,7 @@ jest.setTimeout(737027);
 require("../models/User");
  
 const mongoose = require("mongoose");
-const keys = require("../config/keys");
+const keys = require("./../config/keys");
  
 mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI, {
