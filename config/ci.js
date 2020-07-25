e0701efe90d04efa73a18210b9c5fea45b25be65
djedjethai <djedje.thai.ok@gmail.com>
2020-07-25 17:16:24 +0700

    travis done

--- config/ci.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    '904440237049-5i2g33bjjdlppgdube8rk7s3ebjvsu8m.apps.googleusercontent.com',
-  googleClientSecret: 'e-DZWuPeSmV57AePNlpRgs_N',
-  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci', 
-  cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379'
-};
--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    '904440237049-5i2g33bjjdlppgdube8rk7s3ebjvsu8m.apps.googleusercontent.com',
-  googleClientSecret: 'e-DZWuPeSmV57AePNlpRgs_N',
-  mongoURI: 'mongodb://djedjethai:rootroot@cluster0-shard-00-00-bryms.mongodb.net:27017,cluster0-shard-00-01-bryms.mongodb.net:27017,cluster0-shard-00-02-bryms.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
-  cookieKey: '123123123',
-  redisUrl:'redis://127.0.0.1:6379'
-};
